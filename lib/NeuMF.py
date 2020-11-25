5793544a25dc33c8de9ebc4e2b31baea6ad6c581
Ubuntu <ubuntu@ip-172-31-76-33.ec2.internal>
2020-11-24 16:57:10 -0800

    NeuMF on LastFM

--- lib/NeuMF.py
+++ lib/NeuMF.py
@@ -52,8 +52,8 @@ class DataLoader(torch.utils.data.Dataset):
         self.user_history = []
         self.article_history = []
         self.click_history = []
-        self.size = 65536
-        self.grid = 256
+        self.size = 65536 * 16
+        self.grid = 64
 
     def push(self, user, article, click):
         self.user_history.append(user)
@@ -63,6 +63,7 @@ class DataLoader(torch.utils.data.Dataset):
             self.user_history = self.user_history[self.grid:]
             self.article_history = self.article_history[self.grid:]
             self.click_history = self.click_history[self.grid:]
+            print(len(self.user_history))
 
     def __len__(self):
         return len(self.user_history)
@@ -78,9 +79,8 @@ class DataLoader(torch.utils.data.Dataset):
 class NeuMFYahooAlgorithm(BaseAlg):
     def __init__(self, arg_dict):
         BaseAlg.__init__(self, arg_dict)
-        self.learner = extend(NeuMF(user_dim=5, item_dim=5, mf_dim=8, mlp_dim = [16], lr=1e-2).cuda())
+        self.learner = extend(NeuMF(user_dim=5, item_dim=5, mf_dim=16, mlp_dim = [32, 16, 8], lr=1e-3).cuda())
         self.lossfunc = extend(torch.nn.BCELoss())
-        self.lossfunc_reg = extend(torch.nn.BCELoss())
 
         self.path = './Dataset/Yahoo/YahooKMeansModel/10kmeans_model160.dat'
         self.user_feature = torch.from_numpy(np.genfromtxt(self.path, delimiter=' ')).to(dtype=torch.float).cuda()
@@ -89,6 +89,101 @@ class NeuMFYahooAlgorithm(BaseAlg):
         self.cnt = 0
         self.batch = 1024
 
+        torch.set_num_threads(8)
+        torch.set_num_interop_threads(8)
+
+        self.lamdba = 1
+        self.nu = 0.1
+        self.U = self.lamdba * torch.ones((self.learner.total_param), dtype=torch.float).cuda()
+        self.U1 = torch.zeros((self.learner.total_param), dtype=torch.float).cuda()
+        self.g = None
+        self.reg = None
+        self.t1 = time.time()
+
+
+    def decide(self, pool_articles, userID, k=1):
+        self.a = len(pool_articles)
+        
+        user_vec = torch.cat(self.a*[self.user_feature[userID].view(1, -1)])
+        article_vec = torch.cat([
+            torch.from_numpy(x.contextFeatureVector[:self.dimension]).view(1, -1).to(torch.float32)
+            for x in pool_articles])
+        score = self.learner(user_vec, article_vec.cuda()).view(-1)
+        sum_score = torch.sum(score)
+        with backpack(BatchGrad()):
+            sum_score.backward()
+        
+        grad = torch.cat([p.grad_batch.view(self.a, -1) for p in self.learner.parameters()], dim=1)
+        sigma = torch.sqrt(torch.sum(grad * grad / self.U, dim=1))
+        self.reg = self.nu * torch.mean(sigma).item()
+        arm = torch.argmax(score + self.nu * sigma).item()
+        self.g = grad[arm]
+        return [pool_articles[arm]]
+
+    def updateParameters(self, articlePicked, click, userID):
+        if click == 1 or random.random() < 0.05:
+            user_vec = self.user_feature[userID]
+            article_vec = articlePicked.contextFeatureVector[:self.dimension]
+            assert self.g is not None
+            self.U1 += self.g * self.g
+
+            self.data.push(user_vec, article_vec, click)
+            self.cnt = (self.cnt + 1) % self.batch
+            if self.cnt % self.batch == 0:
+                self.learner.optim.param_groups[0]['weight_decay'] = self.lamdba / len(self.data)
+                t2 = time.time() - self.t1
+                t1 = time.time()
+                dataloader = torch.utils.data.DataLoader(self.data, batch_size=self.batch, shuffle=True, num_workers=0)
+                # self.learner.train().cuda()
+
+                loss_list = []
+                early_cnt = 0
+
+                for i in range(1000):
+                    tot_loss = 0
+                    for j, batch in enumerate(dataloader):
+                        self.learner.optim.zero_grad()
+                        u = batch['user'].cuda()
+                        a = batch['article'].cuda()
+                        c = batch['click'].cuda()
+                        pred = self.learner(u, a).view(-1)
+                        loss = self.lossfunc(pred, c)
+                        tot_loss += loss.item()
+                        loss.backward()
+                        self.learner.optim.step()
+                    # early stopping
+                    if i != 0 and loss_list[-1] < tot_loss / (j + 1):
+                        early_cnt += 1
+                    else:
+                        early_cnt = 0
+
+                    if early_cnt >= 5:
+                        break
+                    loss_list.append(tot_loss / (j + 1))
+
+                # self.learner.eval().cpu()
+                self.U += self.U1
+                self.U1 *= 0
+                print('[{:.2f}, {:.2f} s]: loss: {:.3f}, data: {}, iterations: {}, Covar: {:.2e}, +: [{:.2e}, {:.2e}], -: [{:.2e}, {:.2e}]'.format(
+                    t2, time.time() - t1, loss_list[-1], len(self.data), i + 1, self.reg, 
+                    torch.mean(pred.detach()[c==1]).item(), torch.std(pred.detach()[c==1]).item(),
+                    torch.mean(pred.detach()[c==0]).item(), torch.std(pred.detach()[c==0]).item()))
+                self.t1 = time.time()
+
+
+
+class NeuMFLastFMAlgorithm(BaseAlg):
+    def __init__(self, arg_dict):
+        BaseAlg.__init__(self, arg_dict)
+        self.learner = extend(NeuMF(user_dim=25, item_dim=25, mf_dim=16, mlp_dim = [32, 16, 8], lr=1e-3).cuda())
+        self.lossfunc = extend(torch.nn.BCELoss())
+
+        self.path = './Dataset/LastFM.dat'
+        self.user_feature = torch.from_numpy(np.genfromtxt(self.path, delimiter=' ')).to(dtype=torch.float).cuda()
+
+        self.data = DataLoader()
+        self.cnt = 0
+        self.batch = 64
 
         torch.set_num_threads(8)
         torch.set_num_interop_threads(8)
@@ -104,7 +199,6 @@ class NeuMFYahooAlgorithm(BaseAlg):
 
     def decide(self, pool_articles, userID, k=1):
         self.a = len(pool_articles)
-        t = time.time()
         
         user_vec = torch.cat(self.a*[self.user_feature[userID].view(1, -1)])
         article_vec = torch.cat([
@@ -141,7 +235,7 @@ class NeuMFYahooAlgorithm(BaseAlg):
                 loss_list = []
                 early_cnt = 0
 
-                for i in range(100):
+                for i in range(1000):
                     tot_loss = 0
                     for j, batch in enumerate(dataloader):
                         self.learner.optim.zero_grad()
@@ -166,94 +260,9 @@ class NeuMFYahooAlgorithm(BaseAlg):
                 # self.learner.eval().cpu()
                 self.U += self.U1
                 self.U1 *= 0
-                print('[{:.2f}, {:.2f} s]: loss: {:.3f}, data: {}, iterations: {}, Covar: {:.2f}'.format(
-                    t2, time.time() - t1, loss_list[-1], len(self.data), i + 1, self.reg))
+                print('[{:.2f}, {:.2f} s]: loss: {:.3f}, data: {}, iterations: {}, Covar: {:.2e}, +: [{:.2e}, {:.2e}], -: [{:.2e}, {:.2e}]'.format(
+                    t2, time.time() - t1, loss_list[-1], len(self.data), i + 1, self.reg, 
+                    torch.mean(pred.detach()[c==1]).item(), torch.std(pred.detach()[c==1]).item(),
+                    torch.mean(pred.detach()[c==0]).item(), torch.std(pred.detach()[c==0]).item()))
                 self.t1 = time.time()
 
-
-class NeuMFLastFMAlgorithm(BaseAlg):
-    def __init__(self, arg_dict):
-        BaseAlg.__init__(self, arg_dict)
-        self.learner = NeuMF(user_dim=self.n_users, item_dim=25, mf_dim=8, mlp_dim = [16], lr=1e-3).eval()
-
-        self.data = DataLoader()
-        self.cnt = 0
-        self.batch = 64
-
-        torch.set_num_threads(8)
-        torch.set_num_interop_threads(8)
-
-        self.lamdba = 1e-3
-        self.nu = 1e-2
-        self.U = self.lamdba * torch.ones((self.learner.total_param), dtype=torch.float)
-        self.U1 = torch.zeros((self.learner.total_param), dtype=torch.float)
-        self.g = None
-        self.reg = None
-
-        self.t1 = time.time()
-
-
-    def decide(self, pool_articles, userID, k=1):
-        t1 = time.time()
-        user_vec = torch.zeros((len(pool_articles), self.n_users), dtype=torch.float32)
-        user_vec[:, userID] = 1.0
-        article_vec = torch.cat([
-            torch.from_numpy(x.contextFeatureVector[:self.dimension]).view(1, -1).to(torch.float32)
-            for x in pool_articles])
-        score = self.learner(user_vec, article_vec).view(-1)
-        grad = torch.cat([self.learner.get_grad(x) for x in score])
-        sigma = torch.sqrt(torch.sum(grad * grad / self.U, dim=1))
-        self.reg = torch.mean(sigma).item()
-        arm = torch.argmax(score + self.nu * sigma).item()
-        self.g = grad[arm]
-        arm = torch.argmax(score).item()
-        return [pool_articles[arm]]
-
-    def updateParameters(self, articlePicked, click, userID):
-        if click == 1 or random.random() < 1:
-            user_vec = torch.zeros(self.n_users, dtype=torch.float32)
-            user_vec[userID] = 1.0
-            article_vec = articlePicked.contextFeatureVector[:self.dimension]
-            assert self.g is not None
-            self.U1 += self.g * self.g
-
-            self.data.push(user_vec, article_vec, click)
-            self.cnt = (self.cnt + 1) % self.batch
-            if self.cnt % self.batch == 0:
-                self.learner.optim.param_groups[0]['weight_decay'] = self.lamdba / len(self.data)
-                t2 = time.time() - self.t1
-                t1 = time.time()
-                dataloader = torch.utils.data.DataLoader(self.data, batch_size=self.batch, shuffle=True, num_workers=0)
-                self.learner.train().cuda()
-
-                loss_list = []
-                early_cnt = 0
-
-                for i in range(100):
-                    tot_loss = 0
-                    for j, batch in enumerate(dataloader):
-                        self.learner.optim.zero_grad()
-                        u = batch['user'].cuda()
-                        a = batch['article'].cuda()
-                        c = batch['click'].cuda()
-                        pred = self.learner(u, a).view(-1)
-                        loss = self.learner.loss(pred, c)
-                        tot_loss += loss.item()
-                        loss.backward()
-                        self.learner.optim.step()
-                    # early stopping
-                    # if i != 0 and loss_list[-1] < tot_loss / (j + 1):
-                    #     early_cnt += 1
-                    # else:
-                    #     early_cnt = 0
-
-                    # if early_cnt >= 10:
-                    #     break
-                    loss_list.append(tot_loss / (j + 1))
-
-                self.learner.eval().cpu()
-                self.U += self.U1
-                self.U1 *= 0
-                print('[{:.2f}, {:.2f} s]: loss: {:.3f}, data: {}, iterations: {}, Covar: {:.2f}'.format(
-                    t2, time.time() - t1, loss_list[-1], len(self.data), i + 1, self.reg))
-                self.t1 = time.time()
